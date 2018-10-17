555bfd32862b6f7d7bf95e3a81a9241138348a21
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-10-17 11:21:07 +0000

    hahahahaerzdg

--- P0-HalloHeimur/sketch.js
+++ P0-HalloHeimur/sketch.js
@@ -1,10 +1,11 @@
 function setup(){
     createCanvas(300,300);
-    background(70,200,70);
+    background(70,70,70);
     fill(150);
     ellipse(55,55,77,77);
     fill(0);
     textSize(20);
     text("Halló heimur",100,40);
     rect(30,20,55,55);
+    ellipse(100,200,50,50);
 }
