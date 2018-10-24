3aeb5912e561204f3d6d6432293aa1ac29eda547
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-10-24 10:10:31 +0000

    e

--- P2-Andlit/face.js
+++ P2-Andlit/face.js
@@ -6,6 +6,9 @@ var eyeSize = 20;
 var eyex = 150;
 var eyey = 150;
 var eyedist = 40
+var facecolourb = 170
+var facecolourg = 212
+var facecolourr = 255
 
 function setup(){
   createCanvas (300,300);
@@ -13,9 +16,13 @@ function setup(){
 
 function draw(){
   background(180);
+  fill(facecolourb, facecolourg, facecolourr);
   ellipse(x, y, faceWidth, faceHeight);
+  fill(255)
   ellipse(eyex, eyey, eyeSize, eyeSize)
   ellipse(eyex+eyedist, eyey, eyeSize, eyeSize)
+  ellipse(x, y+40, faceWidth-50, faceHeight-130)
+
   //.....k��i sem sta�setur augun m.v. x, y, faceWidth og faceHeight �tti a� koma h�r.
 
 }
@@ -28,4 +35,7 @@ function mousePressed(){
   eyex       = random(130, 160);
   eyey       = random(145, 155);
   eyedist    = random(20, 40)
+  facecolourb = random(50, 140)
+  facecolourg = random(90, 210)
+  facecolourr = random(100, 255)
 }
