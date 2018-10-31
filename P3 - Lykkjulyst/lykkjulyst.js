04db4cd8e3c93230f0e7c589a01aeccf7860e94e
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-10-31 09:13:36 +0000

    e

--- P3 - Lykkjulyst/lykkjulyst.js
+++ P3 - Lykkjulyst/lykkjulyst.js
@@ -1,14 +1,16 @@
 function setup() {
-  createCanvas(200,300);
+  createCanvas(300,300);
   background(255, 215, 0);
   fill(0);
-  colorMode(HSB);
-  frameRate(5);
+  frameRate(2);
 }
 
 function draw(){
-  for(var i = 0; i < 10 ; i = i + 1) {
-    fill(random(0,360),75,100);
-    ellipse(100, 50 + i*20, 15, 15);
+  background(255, 215, 0);
+  for(var i = 0; i < 15; i = i + 1) {
+    fill((0, 0, 255), 75,100);
+    ellipse(10, 10 + i*20, 7, 7);
+  for(var o = 0; o < random(0, 30); o = o + 1)
+    ellipse(10 + o*20, 10 + i*20, 7, 7);
   }
 }
