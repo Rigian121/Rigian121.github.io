09100bfdab66430479edf303bed1f27842cac3b4
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-10-31 09:34:55 +0000

    e

--- P3b-Lykkjuverk/lykkjuthingy.js
+++ P3b-Lykkjuverk/lykkjuthingy.js
@@ -2,7 +2,6 @@ function setup() {
   createCanvas(300,300);
   background(255, 215, 0);
   fill(0);
-  colorMode(HSB);
   frameRate(2);
 }
 
@@ -11,6 +10,6 @@ function draw(){
     fill(random(0, 360), 75,100);
     ellipse(10, 10 + i*20, 7, 7);
   for(var o = 0; o < random(0, 30); o = o + 1)
-    ellipse(10 + o*20, 10 + i*20, 7, 7
-    }
+    ellipse(10 + o*20, 10 + i*20, 7, 7);
+  }
 }
