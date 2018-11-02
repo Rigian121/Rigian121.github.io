00e155e0e554f95043eb446b84654af59dbcbe6f
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-02 13:25:28 +0000

    e

--- Experiment/e.js
+++ Experiment/e.js
@@ -1,16 +1,11 @@
 function setup() {
-  createCanvas(300,300);
+  createCanvas(1000,1000);
   background(0);
   fill(0);
   frameRate(10);
 }
 
 function draw(){
-background(0);
-    for(var f = 0; f < 14; f = f + 1) {
-      fill(0, 210, 30);
-      rect(10, 10 + f*20, 15, 15);
-    for(var g = 0; g < random(0, 20); g = g + 1)
-      rect(10 + g*20, 10 + f*20, 15, 15);
-  }
+    fill(255)
+    ellipse(100, 100, 100, 100)
 }
