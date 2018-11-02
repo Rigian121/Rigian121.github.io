ff3ce940a66799b11583ebab69935b624c0e4e19
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-02 14:00:39 +0000

    e

--- P3b-Lykkjuverk/lykkjuthingy.js
+++ P3b-Lykkjuverk/lykkjuthingy.js
@@ -2,15 +2,22 @@ function setup() {
   createCanvas(300,300);
   background(0);
   fill(0);
-  frameRate(10);
+  frameRate(5);
 }
 
 function draw(){
 background(0);
     for(var f = 0; f < 14; f = f + 1) {
-      fill(0, 210, 30);
+      fill(0, 210, 0);
       rect(10, 10 + f*20, 15, 15);
     for(var g = 0; g < random(0, 20); g = g + 1)
       rect(10 + g*20, 10 + f*20, 15, 15);
+if(g > 5)
+if(g < 10)
+      fill(210, 210, 0);
+      rect(10 + g*20, 10 + f*20, 15, 15);
+if(g > 10)
+      fill(210, 0, 0);
+      rect(30 + g*20, 10 + f*20, 15, 15);
   }
 }
