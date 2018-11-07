d49d683e7af53293552925e206ad73cb3b603907
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-07 08:43:18 +0000

    e

---
+++ P5-Sprætur/sprætur.js
@@ -0,0 +1,22 @@
+var sloth;
+var owl;
+
+function preload() {
+sloth = loadImage('assets/sloth.png')
+owl = loadImage('assets/owl.png')
+}
+
+function setup() {
+  createCanvas(600,300);
+  background(255,200,0);
+}
+
+function draw(){
+background(0);
+if(mouseX < 300){
+image(owl, mouseX, mouseY)
+  }
+if(mouseX > 300){
+  image(sloth, mouseX, mouseY)
+  }
+}
