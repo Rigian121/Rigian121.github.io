a6c9ef9fcfff11664e59bd7eb65713093a769179
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-07 10:03:28 +0000

    e

--- P6-Skopp/skopp.js
+++ P6-Skopp/skopp.js
@@ -1,19 +1,31 @@
-var sloth;
-var owl;
-
-function preload() {
-sloth = loadImage('assets/sloth.png')
-owl = loadImage('assets/owl.png')
-}
+var x = 100;
+var y = 100;
+var xSpeed = 6;
+var ySpeed = 4;
+var points = 0;
 
 function setup() {
   createCanvas(600,600);
-  background(255,200,0);
+  background(0);
 }
 
-function draw(){
+function draw() {
 background(0);
-  imageMode(CENTER)
-  fill(200)
-  rect(mouseX, 30, 100, 20)
+  rectMode(CENTER)
+  rect(80,250,10,600)
+  rect(510,250,10,600)
+  fill(255)
+  rect(mouseX, 470, 100, 20)
+  rect(x, y, 20, 20)
+  x = x + xSpeed
+  y = y + ySpeed
+  if ((x > 490) || (x < 100)) {
+    xSpeed = xSpeed * -1;
+  }
+  if ((y > 590) || (y < 10)) {
+    ySpeed = ySpeed * -1;
+  }
+  if ((y > 450) && (mouseX < x + 50) && (mouseX > x - 50)) {
+    ySpeed = ySpeed * -1
+  }
 }
