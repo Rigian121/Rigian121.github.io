07f0023ac6d6d641bf526b0ec7228252194e30d0
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-07 08:46:45 +0000

    e

--- P5-Sprætur/spraetur.js
+++ P5-Sprætur/spraetur.js
@@ -9,15 +9,15 @@ owl = loadImage('assets/owl.png')
 function setup() {
   createCanvas(600,300);
   background(255,200,0);
-  imageMode(CENTER)
 }
 
 function draw(){
 background(0);
-if(mouseX < 300){
-image(owl, mouseX, mouseY)
+  imageMode(CENTER)
+  if(mouseX < 300){
+    image(owl, mouseX, mouseY)
   }
-if(mouseX > 300){
-  image(sloth, mouseX, mouseY)
+  if(mouseX > 300){
+    image(sloth, mouseX, mouseY)
   }
 }
