cfaf162c3ef5becdc99eab0ce91c07c77dd70b51
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-14 10:04:42 +0000

    e

--- P7-Fígúra/char.js
+++ P7-Fígúra/char.js
@@ -1,13 +1,14 @@
 function setup() {
   createCanvas(300,300);
+  frameRate(2)
 }
 function draw() {
   background(255);
-  Spodder(80, 80, 50, 70, 0, 80)
-  Spodder(200, 200, 30, 20, 0, 200)
+  Bug(80, 80, 50, 70, 0, 80)
+  Bug(200, 200, random(10,100), random(10,100), 0, random(0,255))
 }
 
-function Spodder(x,y, bodyWidth, bodyLength, legLength, bodyShade){
+function Bug(x,y, bodyWidth, bodyLength, legLength, bodyShade){
   fill(bodyShade)
   ellipse(x, y, bodyWidth, bodyLength)
   ellipse(x, y-bodyLength/2, bodyWidth/1.5, bodyWidth/1.5)
