c22604bf538c228f37a68095edd60035ab68c76b
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-14 08:51:39 +0000

    e

--- P6-Skopp/skopp.js
+++ P6-Skopp/skopp.js
@@ -1,14 +1,15 @@
 var x = 100;
 var y = 100;
 var xSpeed = 6;
-var ySpeed = 4;
+var ySpeed = 6;
 var spadiX = 300
 var spadiBreidd = 100;
+var spadiThykkt = 20;
 var score = 0;
+var lives = 2
 
 function setup() {
   createCanvas(600,600);
-  background(0);
 }
 
 function draw() {
@@ -17,7 +18,7 @@ background(0);
   rect(80,250,10,600)
   rect(510,250,10,600)
   fill(255)
-  rect(mouseX, 470, spadiBreidd, 20)
+  rect(mouseX, 470, spadiBreidd, spadiThykkt)
   rect(x, y, 20, 20)
   x = x + xSpeed
   y = y + ySpeed
@@ -30,7 +31,7 @@ background(0);
   if ((x > 490) || (x < 100)) {
     xSpeed = xSpeed * -1;
   }
-  if ((y > 590) || (y < 10)) {
+  if (y < 10) {
     ySpeed = ySpeed * -1;
   }
   if (y < 471){
@@ -39,23 +40,34 @@ background(0);
       score = score + 1
     }
     if ((y > 450) && (mouseX < x) && (mouseX > x - spadiBreidd/4)) {
-      xSpeed = 3
-      ySpeed = -4
+      xSpeed = 6
+      ySpeed = -8
     }
     if ((y > 450) && (mouseX < x - spadiBreidd/4) && (mouseX > x - spadiBreidd/2)) {
-      xSpeed = 6
-      ySpeed = -2
-    }
-    if ((y > 450) && (mouseX < x + spadiBreidd/4) && (mouseX > x)) {
-      xSpeed = -3
+      xSpeed = 8
       ySpeed = -4
     }
-    if ((y > 450) && (mouseX < x + spadiBreidd/2) && (mouseX > x + spadiBreidd/4)) {
+    if ((y > 450) && (mouseX < x + spadiBreidd/4) && (mouseX > x)) {
       xSpeed = -6
-      ySpeed = -2
+      ySpeed = -8
     }
+    if ((y > 450) && (mouseX < x + spadiBreidd/2) && (mouseX > x + spadiBreidd/4)) {
+      xSpeed = -8
+      ySpeed = -4
+    }
+  }
+  if (y > 610 && lives >= 0){
+    lives = lives - 1
+    x = 100
+    y = 100
+  }
+  if(lives == -1){
+    text("Game Over", 270, 300)
+    y = 611
   }
   //if ((y < 470) && (y > 450) && (mouseX = x - spadiBreidd/2) && (mouseX = x + spadiBreidd/2)) {
     //xSpeed = xSpeed * -1;
   //}
+	text("Score: " + score,10,20);
+  text("Lives: " + lives, 10, 40)
 }
