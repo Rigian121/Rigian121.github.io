ea796199cbf01c1aefcff705411a7e942f790159
Rigian121 <44091980+Rigian121@users.noreply.github.com>
2018-11-19 12:14:09 +0000

    e

--- P7-Fígúra/char.js
+++ P7-Fígúra/char.js
@@ -4,11 +4,11 @@ function setup() {
 }
 function draw() {
   background(255);
-  Buggie(80, 80, 50, 70, 0, 80)
-  Buggie(200, 200, random(10,100), random(30,130), 0, random(0,255))
+  Buggie(80, 80, 50, 70, 80)
+  Buggie(200, 200, random(10, 50), random(50, 80), random(0, 255))
 }
 
-function Buggie(x,y, bodyWidth, bodyLength, legLength, bodyShade){
+function Buggie(x,y, bodyWidth, bodyLength, bodyShade){
   fill(bodyShade)
   ellipse(x, y, bodyWidth, bodyLength)
   ellipse(x, y-bodyLength/2, bodyWidth/1.5, bodyWidth/1.5)
@@ -16,4 +16,8 @@ function Buggie(x,y, bodyWidth, bodyLength, legLength, bodyShade){
   line(x+bodyWidth, y-bodyWidth/4, x+bodyWidth*1.5, y-bodyWidth)
   line(x-bodyWidth/2, y, x-bodyWidth, y-bodyWidth/4)
   line(x-bodyWidth, y-bodyWidth/4, x-bodyWidth*1.5, y-bodyWidth)
+  line(x+bodyWidth/2.35, y+bodyLength/4, x+bodyWidth, y+bodyWidth/2)
+  line(x+bodyWidth, y+bodyWidth/2, x+bodyWidth*1.5, y+bodyWidth)
+  line(x-bodyWidth/2.35, y+bodyLength/4, x-bodyWidth, y+bodyWidth/2)
+  line(x-bodyWidth, y+bodyWidth/2, x-bodyWidth*1.5, y+bodyWidth)
 }
